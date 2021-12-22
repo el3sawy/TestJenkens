a81c2d045898fe62bd47f59b47b985f011a7a6d3
Naguib <ahmed.naguibeleissawy@vodafone.com>
2021-12-22 15:30:59 +0200

    init

---
+++ TestJenkens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestJenkens
+//
+//  Created by Ahmed Elesawy on 22/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
