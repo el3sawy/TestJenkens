a17132e2a0a2a4bf04a967d44ccf4664d386c5e8
Naguib <ahmed.naguibeleissawy@vodafone.com>
2021-12-27 15:39:32 +0200

    code coberrage

--- TestJenkens/ViewController.swift
+++ TestJenkens/ViewController.swift
@@ -11,7 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        AF.request(URL(string: "")!).response { _ in
+            
+        }
     }
 
 
